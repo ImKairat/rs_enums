399f423c5852b39129e78536f77c324b9b82d79b
Kubanychbek uulu Kairat <kubanychbekukairat@gmail.com>
2024-12-03 16:49:41 +0600

    [REFACTORED] to comply with Pylint standards.

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+"""
+Setup configuration for the my_package project.
+This module uses setuptools to package the application.
+"""
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",  
-    version="0.1.0",    
-    packages=find_packages(),  
+    name="my_package",
+    version="0.1.0",
+    packages=find_packages(),
     install_requires=[
-        
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.11',  
-)
+    python_requires='>=3.11',
+    )
